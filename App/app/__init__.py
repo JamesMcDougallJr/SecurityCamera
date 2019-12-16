45c97472f43689bb9759287d6d84aab94fa99333
JamesMcDougallJr <jmcdouga@ucsd.edu>
2019-12-15 17:14:34 -0800

    Moved test images around, requirements.txt

--- App/app/__init__.py
+++ App/app/__init__.py
@@ -7,6 +7,4 @@ app = flask.Flask(__name__)
 if 'DYNO' in os.environ: # only trigger SSLify if the app is running on Heroku
     sslify = SSLify(app)
 
-db = SQLAlchemy(app)  # type: SQLAlchemy
-
 from app.web import routes
--- App/app/web/routes.py
+++ App/app/web/routes.py
@@ -1,16 +1,18 @@
+import os
 import flask
+from importlib import import_module
 from app import app
 
 # import camera driver
 if os.environ.get('CAMERA'):
     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
 else:
-    from camera import Camera
+    from ..backend.camera import Camera
 
 @app.route('/')
 def index():
     """Video streaming home page."""
-    return render_template('index.html')
+    return flask.render_template('root.html')
 
 
 def gen(camera):
@@ -18,11 +20,11 @@ def gen(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),
+    return flask.Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
